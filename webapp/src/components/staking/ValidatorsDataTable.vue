16c030fbb7e42425f100b6539e67166ee96b3a8b
Pawel Borecki <pawel.borecki@ovoo.pl>
2022-08-01 12:00:14 +0200

    fix after merge

--- webapp/src/components/staking/ValidatorsDataTable.vue
+++ webapp/src/components/staking/ValidatorsDataTable.vue
@@ -1,73 +1,74 @@
 <template>
-  <StakingPopup :validator="currentValidator" v-if="popupOpened" @success="trsansactionSuccess" @close="checkBTN"/>
-  <DataTableWrapper :data-key="'operator_address'" :useExternalGlobalFilter="false" :eager-loading-config="createEagerLoadingConfig()" :expanded-rows="expandedRow" @row-click="onRowClick">
-    <template v-slot:empty>{{ $t("STAKING_VIEW.NO_VALIDATORS") }}</template>
-    <template #header>
-      <div style="display: flex; justify-content: space-between">
-        <h5 class="m-0">{{ $t("STAKING_VIEW.VALIDATORS") }}</h5>
-              <span class="p-input-icon-left">
-                <i class="pi pi-search" />
-                <InputText type="text" v-model="filters['global'].value" placeholder="Search" />
-                <i class="pi pi-times-circle" @click="filters['global'].value = ''"/>
-             </span>
-      </div>
-    </template>
-    <template v-slot:columns>
-      <Column field="rank" :header="$t(`STAKING_VIEW.TABLE_HEADERS.RANK`)" :sortable="true"></Column>
-      <Column field="description.moniker" :header="$t(`STAKING_VIEW.TABLE_HEADERS.NAME`)" :sortable="true"></Column>
-      <Column field="status" :header="$t(`STAKING_VIEW.TABLE_HEADERS.STATUS`)" :sortable="true">
-        <template #body="{data}">
-          <span>{{ data.viewStatus }}</span>
-        </template>
-      </Column>
-      <Column field="commission.rate" header="Commission" :sortable="true" sortField="commission.rate">
-        <template #body="{data}">
-          <span>{{ data.commission.rateViewPercentage }}%</span>
-        </template>
-      </Column>
-      <Column field="votingPower" :header="$t(`STAKING_VIEW.TABLE_HEADERS.VOTING_POWER`)" :sortable="true" sortField="tokens">
-        <template #body="{data}">
-          <span v-if="data.votingPower">{{ data.votingPowerViewPercentage }}%</span> 
-          <span v-else>updating</span>
-        </template>
-      </Column>
-      <Column :header="$t(`STAKING_VIEW.TABLE_HEADERS.STAKE`)" :sortable="true" v-if="isLoggedIn" sortField="delegatedAmount">
-        <template #body="{data}">
-          <span>{{ data.delegatedViewAmount }}</span>
-          <!-- <span v-else>updating</span> -->
-        </template>
-      </Column>
-      <Column field="operator_address">
-        <template #body="{data}">
-          <Button class="outlined" @click="checkBTN(data)">{{ $t(`STAKING_VIEW.TABLE_BUTTONS.MANAGE_BTN`) }}</Button>
-        </template>
-      </Column>
-
-      <Column v-if="isLoggedIn">
-        <template #body="{data}">
-          <span style="cursor: pointer" @click="onRowExpand(data)" v-if="isValidatorRowExpandable(data)">
-            <Icon @click="onRowExpand(data)" v-if="data.operatorAddress != expandedRow[0]?.operatorAddress" name="ChevronRight" />
-            <Icon @click="onRowExpand(data)" v-if="data.operatorAddress == expandedRow[0]?.operatorAddress" name="ChevronDown" />
-          </span>
-        </template>
-      </Column>
-
-    </template>
-    <template v-slot:expanded-columns="{expandedData}">
-      <div style="display: flex; flex-direction: row;">
-        <div style="display: flex; flex-direction: column; margin-right: 20px">
-          <p>{{ $t(`STAKING_VIEW.TABLE_EXPANDED.UNSTAKING`) }}</p>
-          <p>{{ expandedData.data.undelegatingViewAmount }}</p>
+  <span>
+    <StakingPopup :validator="currentValidator" v-if="popupOpened" @success="trsansactionSuccess" @close="checkBTN"/>
+    <DataTableWrapper :data-key="'operator_address'" :useExternalGlobalFilter="false" :eager-loading-config="createEagerLoadingConfig()" :expanded-rows="expandedRow" @row-click="onRowClick">
+      <template v-slot:empty>{{ $t("STAKING_VIEW.NO_VALIDATORS") }}</template>
+      <template #header>
+        <div style="display: flex; justify-content: space-between">
+          <h5 class="m-0">{{ $t("STAKING_VIEW.VALIDATORS") }}</h5>
+                <span class="p-input-icon-left">
+                  <i class="pi pi-search" />
+                  <InputText type="text" v-model="filters['global'].value" placeholder="Search" />
+                  <i class="pi pi-times-circle" @click="filters['global'].value = ''"/>
+              </span>
         </div>
-        <div style="display: flex; flex-direction: column">
-          <p>{{ $t(`STAKING_VIEW.TABLE_EXPANDED.REWARDS`) }}</p>
-          <p>{{ expandedData.data.rewardsViewAmount }}</p> <!-- TODONUMBER -->
+      </template>
+      <template v-slot:columns>
+        <Column field="rank" :header="$t(`STAKING_VIEW.TABLE_HEADERS.RANK`)" :sortable="true"></Column>
+        <Column field="description.moniker" :header="$t(`STAKING_VIEW.TABLE_HEADERS.NAME`)" :sortable="true"></Column>
+        <Column field="status" :header="$t(`STAKING_VIEW.TABLE_HEADERS.STATUS`)" :sortable="true">
+          <template #body="{data}">
+            <span>{{ data.viewStatus }}</span>
+          </template>
+        </Column>
+        <Column field="commission.rate" header="Commission" :sortable="true" sortField="commission.rate">
+          <template #body="{data}">
+            <span>{{ data.commission.rateViewPercentage }}%</span>
+          </template>
+        </Column>
+        <Column field="votingPower" :header="$t(`STAKING_VIEW.TABLE_HEADERS.VOTING_POWER`)" :sortable="true" sortField="tokens">
+          <template #body="{data}">
+            <span v-if="data.votingPower">{{ data.votingPowerViewPercentage }}%</span> 
+            <span v-else>updating</span>
+          </template>
+        </Column>
+        <Column :header="$t(`STAKING_VIEW.TABLE_HEADERS.STAKE`)" :sortable="true" v-if="isLoggedIn" sortField="delegatedAmount">
+          <template #body="{data}">
+            <span>{{ data.delegatedViewAmount }}</span>
+            <!-- <span v-else>updating</span> -->
+          </template>
+        </Column>
+        <Column field="operator_address">
+          <template #body="{data}">
+            <Button class="outlined" @click="checkBTN(data)">{{ $t(`STAKING_VIEW.TABLE_BUTTONS.MANAGE_BTN`) }}</Button>
+          </template>
+        </Column>
+
+        <Column v-if="isLoggedIn">
+          <template #body="{data}">
+            <span style="cursor: pointer" @click="onRowExpand(data)" v-if="isValidatorRowExpandable(data)">
+              <Icon @click="onRowExpand(data)" v-if="data.operatorAddress != expandedRow[0]?.operatorAddress" name="ChevronRight" />
+              <Icon @click="onRowExpand(data)" v-if="data.operatorAddress == expandedRow[0]?.operatorAddress" name="ChevronDown" />
+            </span>
+          </template>
+        </Column>
+
+      </template>
+      <template v-slot:expanded-columns="{expandedData}">
+        <div style="display: flex; flex-direction: row;">
+          <div style="display: flex; flex-direction: column; margin-right: 20px">
+            <p>{{ $t(`STAKING_VIEW.TABLE_EXPANDED.UNSTAKING`) }}</p>
+            <p>{{ expandedData.data.undelegatingViewAmount }}</p>
+          </div>
+          <div style="display: flex; flex-direction: column">
+            <p>{{ $t(`STAKING_VIEW.TABLE_EXPANDED.REWARDS`) }}</p>
+            <p>{{ expandedData.data.rewardsViewAmount }}</p> <!-- TODONUMBER -->
+          </div>
         </div>
-      </div>
-    </template>
-<!--    <template v-slot:paginatorstart></template>-->
-  </DataTableWrapper>
-</span>
+      </template>
+  <!--    <template v-slot:paginatorstart></template>-->
+    </DataTableWrapper>
+  </span>
 </template>
 
 <script setup lang="ts">
