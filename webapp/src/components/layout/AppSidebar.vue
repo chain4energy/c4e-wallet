f9a38caeb4656bb2e59f038763c710e9d2737773
dlavrentiev <3354678Ltybc>
2022-08-03 11:53:48 +0200

    previos works but restore Dominiks solution

--- webapp/src/components/layout/AppSidebar.vue
+++ webapp/src/components/layout/AppSidebar.vue
@@ -5,9 +5,9 @@
       <Icon name="SidebarOpen"/>
     </div>
     <nav class="sidebar display-none">
-      <router-link :to="menuItem.href" v-for="(menuItem,index) of menu" @click="changeSelected(index)" :key="menuItem">
+      <router-link :to="menuItem.href" v-for="(menuItem,index) of menu" @click="changeSelected(index)" :key="index">
         <span class="sidebar-element">
-          <span class="icon" :class="{ 'active': index === selected }">
+          <span :class="menuItem.href === this.$route.path ? 'icon active' : 'icon'">
             <Icon :name="menuItem.icon.element"/>
           </span>
           <span class="title">{{ menuItem.title }}</span>
@@ -22,17 +22,32 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from "vue";
-// import 'vue-sidebar-menu/dist/vue-sidebar-menu.css';
+import { computed, ref } from "vue";
 import Icon from "../features/IconComponent.vue";
-import {PermissionsService} from "../../services/permissions/permissions.service";
+import {PermissionsService} from "@/services/permissions/permissions.service";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+const selected = ref(0);
 
 const permissionsService = new PermissionsService();
-const menu = computed(() => {
-  console.log(permissionsService.createSideBar());
-  return permissionsService.createSideBar();
+const menu = computed(() =>{
+  const temp = permissionsService.createSideBar();
+
+  if(route.name != undefined) {
+    const sidebarElement = temp.find(element => element.href == "/" + route.name?.toString());
+    if(sidebarElement !== undefined) {
+      // TODO: czy da się to zrobić inaczej
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      selected.value = temp.indexOf(sidebarElement);
+    }else{
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      selected.value =-1;
+    }
+  }
+  return temp;
 });
-const selected = ref(0);
 
 function showSidebar() {
   let sidebar = document.getElementsByClassName('sidebar')[0] as HTMLElement;
