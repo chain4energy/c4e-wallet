9aed1bd90451171fd6eb5d7720e530ac80702676
Pawel Borecki <pawel.borecki@ovoo.pl>
2022-08-17 22:46:04 +0200

    spinnner update

--- webapp/src/components/commons/C4ESpinner.vue
+++ webapp/src/components/commons/C4ESpinner.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="frame" >
     <div style="position: relative;">
-      <C4EIcon icon="c4e-4-only" size="160" style="position: absolute;"/>
       <div style="position: absolute;">
-        <div class="lds-roller"><div class="c4e-dots"></div><div class="c4e-dots"></div><div class="c4e-dots"></div><div class="c4e-dots"></div></div>
+        <div class="anim-logo"><C4EIcon icon="c4e-4-only" size="160"/></div>
+      </div>
+      <div style="position: absolute;">
+        <div class="anim-dots"><div class="c4e-dots"></div><div class="c4e-dots"></div><div class="c4e-dots"></div><div class="c4e-dots"></div></div>
       </div>
     </div>
   </div>
@@ -14,23 +16,50 @@ import C4EIcon from './C4EIcon.vue';
 
 </script>
 <style scoped>
+.anim-logo {
+  display: inline-block;
+  transform: translateZ(1px);
+}
+.anim-logo > div {
+  display: inline-block;
+  width: 160px;
+  height: 160px;
+  border-radius: 50%;
+  animation: anim-logo 2.4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+@keyframes anim-logo {
+  0%, 100% {
+    animation-timing-function: cubic-bezier(0.5, 0, 1, 0.5);
+  }
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(1800deg);
+    animation-timing-function: cubic-bezier(0, 0.5, 0.5, 1);
+  }
+  100% {
+    transform: rotateY(3600deg);
+  }
+}
+
 
 .frame {
-    width: 160px;
+  width: 160px;
   height: 160px;
 }
 
-.lds-roller {
+.anim-dots {
   display: inline-block;
   position: relative;
   width: 160px;
   height: 160px;
 }
-.lds-roller div {
-  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+.anim-dots div {
+  animation: anim-dots 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   transform-origin: 80px 80px;
 }
-.lds-roller div:after {
+.anim-dots div:after {
   content: " ";
   display: block;
   position: absolute;
@@ -40,65 +69,38 @@ import C4EIcon from './C4EIcon.vue';
 
   margin: -4px 0 0 -4px;
 }
-.lds-roller div:nth-child(1) {
+.anim-dots div:nth-child(1) {
   animation-delay: -0.036s;
 }
-.lds-roller div:nth-child(1):after {
+.anim-dots div:nth-child(1):after {
   top: 126px;
   left: 126px;
 }
-.lds-roller div:nth-child(2) {
+.anim-dots div:nth-child(2) {
   animation-delay: -0.072s;
 }
-.lds-roller div:nth-child(2):after {
+.anim-dots div:nth-child(2):after {
   /* top: 136px;
   left: 112px; */
   top: 144px;
   left: 96px;
 }
-.lds-roller div:nth-child(3) {
+.anim-dots div:nth-child(3) {
   animation-delay: -0.108s;
 }
-.lds-roller div:nth-child(3):after {
+.anim-dots div:nth-child(3):after {
   top: 146px;
   left: 60px;
 }
-.lds-roller div:nth-child(4) {
+.anim-dots div:nth-child(4) {
   animation-delay: -0.144s;
 }
-.lds-roller div:nth-child(4):after {
+.anim-dots div:nth-child(4):after {
   top: 134px;
   left: 28px;
 }
-/* .lds-roller div:nth-child(5) {
-  animation-delay: -0.18s;
-}
-.lds-roller div:nth-child(5):after {
-  top: 71px;
-  left: 32px;
-}
-.lds-roller div:nth-child(6) {
-  animation-delay: -0.216s;
-}
-.lds-roller div:nth-child(6):after {
-  top: 68px;
-  left: 24px;
-}
-.lds-roller div:nth-child(7) {
-  animation-delay: -0.252s;
-}
-.lds-roller div:nth-child(7):after {
-  top: 63px;
-  left: 17px;
-}
-.lds-roller div:nth-child(8) {
-  animation-delay: -0.288s;
-}
-.lds-roller div:nth-child(8):after {
-  top: 56px;
-  left: 12px;
-} */
-@keyframes lds-roller {
+
+@keyframes anim-dots {
   0% {
     transform: rotate(0deg);
   }
