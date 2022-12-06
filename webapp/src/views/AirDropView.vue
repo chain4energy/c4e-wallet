b969c61a950ad2032f98cd98bf596b0a184080fe
DenisPM <lavrentiev@bramblingtech.com>
2022-12-06 15:04:53 +0100

    airdrop completed

--- webapp/src/views/AirDropView.vue
+++ webapp/src/views/AirDropView.vue
@@ -1,44 +1,104 @@
 <template>
 <div class="tile">
-  <TabView style="width: 100%">
-    <TabPanel style="width: 100%">
+  <TabView class="airDrop__tabview">
+    <TabPanel >
       <template #header>
         <div>Cosmos Air Drop</div>
       </template>
-      <div class="airDrop__login">
-        <h3>To see airdrops please provide</h3>
-        <div class="airDrop__login-address">
-          <Form @submit="submit" :validation-schema="amountSchema" v-slot="{ errors }" class="airDrop__login-form">
-            <p class="airDrop__text"> Cosmos or C4E Address :</p>
+      <div class="airDrop">
+        <div class="airDrop__container">
+          <Button v-if="!userLoggedIn" class="airDrop__btn" @click="dataService.onKeplrLogIn()">
+            <KeplrLogo/> {{ $t('AIRDROP.CONNECT' )}}
+          </Button>
+          <hr v-if="!userLoggedIn" :data-after="$t('AIRDROP.OR')"/>
+          <p class="airDrop__text">{{$t('AIRDROP.CONNECT_CHAINS')}}</p>
+          <Form @submit="submit" class="airDrop__form">
             <div class="field airDrop__field">
-                <Field v-model="address" name="address" placeholder=" " type="text" class="form-control" style="width: 100%;" :class="{ 'is-invalid': errors.address }"></Field>
-                <span>{{$t('CONNECT.ADDRESS_HELP')}}</span>
-                <div class="invalid-feedback">
-                  {{ errors.address ? errors.address : "" }}
-                </div>
+              <Field  v-model="c4eAddress" name="c4eaddress" placeholder=" " type="text" class="form-control" style="width: 100%;" :class="{ 'is-invalid': errorMessageType.c4e }"></Field>
+              <span class="">{{$t('AIRDROP.C4E_HELP')}}</span>
+              <div class="invalid-feedback">
+                {{ errorMessageType.c4e ? errorMessageType.c4e : "" }}
               </div>
-            <Button class="airDrop__btn" :label="$t('COMMON.CONNECT')" type="submit"></Button>
-          </Form>
-          <div class="airDrop__login-keplr">
-              <p>or</p>
-              <Button @click="dataService.onKeplrLogIn()">
-                <KeplrLogo/> {{ $t('CONNECT.CONNECT' )}}
-              </Button>
             </div>
+            <div class="field airDrop__field">
+              <Field v-model="cosmAddress" name="cosmaddress" placeholder=" " type="text" class="form-control airDrop__input" style="width: 100%;" :class="{ 'is-invalid': errorMessageType.cosm }"></Field>
+              <span class="">{{$t('AIRDROP.COSMOS_HELP')}}</span>
+              <div class="invalid-feedback">
+                {{ errorMessageType.cosm ? errorMessageType.cosm : "" }}
+              </div>
+            </div>
+            <div>
+              <Button type="submit" :label="'Submit'"></Button>
+              <Button type="reset" @click="useAirDropStore().$reset()" :label="'Reset'"></Button>
+            </div>
+          </Form>
+          <p style="
+            width: 90%;
+            text-align: left;
+            ">Total AirDrop allocation
+          </p>
+          <div class="airDrop__result">
+            <div>
+              <Image class="navbar-brand" :src="require('@/assets/c4elogo-new.svg')" alt="Image" height="36" />
+            </div>
+            <CoinAmount
+              :amount="airdropExist ? airDrop1.total_amount : new DecCoin(0, 'c4e')" :precision="2"
+              :show-denom="true"
+              :reduce-big-number="true"
+            />
+          </div>
         </div>
+
+<!--        <Form ref="formCoins" @submit="encodeAddress(c4eAddress || cosmAddress)" v-slot="{ errors }">-->
+<!--          <div class="field airDrop__field">-->
+<!--            <Field type="text" name="cosmAddress" v-model="c4eAddress" placeholder=" " class="form-control" style="width: 100%;" :class="{ 'is-invalid': errors.cosmAddress }"/>-->
+<!--            <div class="invalid-feedback">-->
+<!--              {{ errors.cosmAddress ? errors.cosmAddress : "" }}-->
+<!--            </div>-->
+<!--            <Field type="text" name="c4eAddress" v-model="cosmAddress" placeholder=" " class="form-control" style="width: 100%;" :class="{ 'is-invalid': errors.c4eAddress }"/>-->
+<!--            <div class="invalid-feedback">-->
+<!--              {{ errors.c4eAddress ? errors.c4eAddress : "" }}-->
+<!--            </div>-->
+<!--          </div>-->
+
+<!--        </Form>-->
+
+<!--        <div class="airDrop__login">-->
+<!--          <h3>To see airdrops please provide</h3>-->
+<!--          <div class="airDrop__login-address">-->
+<!--            <Form @submit="submit" :validation-schema="amountSchema" v-slot="{ errors }" class="airDrop__login-form">-->
+<!--              <p class="airDrop__text"> Cosmos or C4E Address :</p>-->
+<!--              <div class="field airDrop__field">-->
+<!--                <Field v-model="address" name="address" placeholder=" " type="text" class="form-control" style="width: 100%;" :class="{ 'is-invalid': errors.address }"></Field>-->
+<!--                <span>{{$t('CONNECT.ADDRESS_HELP')}}</span>-->
+<!--                <div class="invalid-feedback">-->
+<!--                  {{ errors.address ? errors.address : "" }}-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <Button class="airDrop__btn" :label="$t('COMMON.CONNECT')" type="submit"></Button>-->
+<!--            </Form>-->
+<!--            <div class="airDrop__login-keplr">-->
+<!--              <p>or</p>-->
+<!--              <Button @click="dataService.onKeplrLogIn()">-->
+<!--                <KeplrLogo/> {{ $t('CONNECT.CONNECT' )}}-->
+<!--              </Button>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div v-if="airdropExist && fetched">-->
+<!--          <div class="airDrop__congrats">-->
+<!--            <h4>Congratulations your account</h4> <p>{{airDrop1.c4e_address}}</p> can receive-->
+<!--            <CoinAmount :amount="airDrop1.total_amount" :precision="2" :show-denom="true" :reduce-big-number="true"/>-->
+<!--          </div>-->
+<!--          <Button :disabled="true" :label="'claim'"></Button>-->
+<!--        </div>-->
+<!--        <div v-else-if="!airdropExist && fetched">-->
+<!--          <div class="airDrop__congrats">-->
+<!--            <p> There is no airDrop found Try to use another account </p>-->
+<!--          </div>-->
+<!--        </div>-->
       </div>
-      <div v-if="airdropExist && fetched">
-        <div class="airDrop__congrats">
-          <h4>Congratulations your account</h4> <p>{{airDrop1.c4e_address}}</p> can receive
-          <CoinAmount :amount="airDrop1.total_amount" :precision="2" :show-denom="true" :reduce-big-number="true"/>
-        </div>
-        <Button :disabled="true" :label="'claim'"></Button>
-      </div>
-      <div v-else-if="!airdropExist && fetched">
-        <div class="airDrop__congrats">
-          <p> There is no airDrop found Try to use another account </p>
-        </div>
-      </div>
+
     </TabPanel>
 <!--    <TabPanel style="width: 100%">-->
 <!--      <template #header>-->
@@ -87,7 +147,7 @@
 import TabView from "primevue/tabview";
 import TabPanel from "primevue/tabpanel";
 import {useUserStore} from "@/store/user.store";
-import {computed, ref, watch} from "vue";
+import {computed, onMounted, reactive, ref, watch} from "vue";
 import dataService from "@/services/data.service";
 import KeplrLogo from "@/components/commons/KeplrLogo.vue";
 import {useAirDropStore} from "@/store/airDrop.store";
@@ -101,107 +161,210 @@ import * as bench32 from "bech32";
 import {YupSequentialStringSchema} from "@/utils/yup-utils";
 import {Field, Form} from "vee-validate";
 import Button from "primevue/button";
+import {useI18n} from "vue-i18n";
+
+const airDropStore = useAirDropStore()
+
+
+// function encodeAddress(address : string | undefined){
+//   let decoded;
+//   if (address != null) {
+//     decoded = bech32.decode(address);
+//   }
+//   if(decoded){
+//     c4eAddress.value =  bech32.encode("c4e", decoded.words);
+//     cosmAddress.value = bech32.encode("cosmos", decoded.words);
+//     return true;
+//   } else return false;
+//
+// }
+const c4eAddress = ref();
+const cosmAddress = ref();
+const address = ref();
+let errorMessageType = ref({
+  c4e: '',
+  cosm: ''
+});
+onMounted(() => {
+  if(useUserStore().getAccount.address != ''){
+    c4eAddress.value = useUserStore().getAccount.address;
+  }
+});
+
+
+
+// function changeValueChain(address: string, chain: string){
+//   const decoded = bech32.decode(address);
+//   if(decoded){
+//     return bech32.encode(chain, decoded.words);
+//   } else {
+//     return '';
+//   }
+// }
+// function formChanged(e){
+//   console.log(e)
+//   if(bech32.decode(c4eAddress.value) || bech32.decode(cosmAddress.value)){
+//     cosmAddress.value = changeValueChain(c4eAddress.value, 'cosmos');
+//     c4eAddress.value = changeValueChain(cosmAddress.value, 'c4e');
+//   }
+//
+// }
+
+
+watch(c4eAddress , (next)=>{
+  try{
+    address.value = bech32.decode(next);
+    fetchVal();
+    errorMessageType.value.c4e = '';
+  } catch (e){
+    errorMessageType.value.c4e = e;
+  }
+
+});
+
+watch(cosmAddress , (next)=>{
+  try{
+    address.value = bech32.decode(next);
+    fetchVal();
+    errorMessageType.value.cosm = '';
+  } catch (e){
+    errorMessageType.value.cosm = e;
+  }
+});
+
+
+function fetchVal(){
+  c4eAddress.value = bech32.encode("c4e", address.value.words);
+  cosmAddress.value = bech32.encode("cosmos", address.value.words);
+}
 
 const fetched = ref(false);
 
-const userStore = useUserStore();
 
+function submit() {
+  if(errorMessageType.value.c4e =='' && errorMessageType.value.cosm ==''){
+    useAirDropStore().fetchAirdrop(bech32.encode("c4e", address.value.words)).then(() => {
+      fetched.value = true;
+    });
+  }
+}
+
+//
+// const userStore = useUserStore();
+// const airDropStore = useAirDropStore()
+//
 const userLoggedIn = computed(() =>{
-  return userStore.getAccount.address != '';
+  return useUserStore().getAccount.address != '';
 });
+
+watch(userLoggedIn, () => {
+  c4eAddress.value = useUserStore().getAccount.address;
+})
 const airdropExist = computed(() => {
-  return useAirDropStore().getAirDropStatus;
+  return airDropStore.getAirDropStatus;
 });
 
 const airDrop1= computed(() =>{
-  return useAirDropStore().getAirDrop;
+  return airDropStore.getAirDrop;
 });
+//
+// const address = ref(useUserStore().getAccount.address);
+// // const c4eAddress = ref();
+// let errorMessageType = '';
+//
+// // if(useUserStore().getAccount.address != ''){
+// //   useAirDropStore().fetchAirdrop(useUserStore().getAccount.address, true);
+// //   fetched.value=true;
+// // }
+//
+//
 
-const address = ref(useUserStore().getAccount.address);
-const c4eAddress = ref();
-let errorMessageType = '';
 
-if(useUserStore().getAccount.address != ''){
-  useAirDropStore().fetchAirdrop(useUserStore().getAccount.address, true);
-  fetched.value=true;
-}
 
-function encodeAddress(address: string){
-  const decoded = bech32.decode(address);
-  console.log(decoded);
-  c4eAddress.value = bech32.encode("c4e", decoded.words);
-  useAirDropStore().fetchAirdrop(c4eAddress.value, true);
-  fetched.value=true;
-}
 
-async function validateAddress(address: string | undefined){
-  if (!address) {
-    errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.EMPTY');
-    return false;
-  }
-  try {
-    const words = bench32.decode(address);
-    if(words?.prefix !== useConfigurationStore().config.addressPrefix){
-      errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.NOT_THIS_NETWORK', {prefix: useConfigurationStore().config.addressPrefix});
-    }
-    return true
-
-  } catch (err) {
-    onWrongAddress(address, String(err));
-    return false;
-  }
-}
-
-function onWrongAddress(address: string, err: string) {
-  console.log(err.slice(7));
-  switch (err.slice(7)){
-    case address + ' too short' || 'Data too short':
-      errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.TOO_SHORT');
-      break;
-    case 'No separator character for '+ address:
-      errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.SEPARATOR');
-      break;
-    case 'Invalid checksum for '+ address:
-      errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.CHECK_SUM');
-      break;
-    default:
-      errorMessageType = i18n.global.t('CONNECT.ADDRESS_VALIDATION.INVALID');
-      break;
-  }
-}
-const amountSchema = object().shape({
-  address: YupSequentialStringSchema([
-    string().required(i18n.global.t('CONNECT.ADDRESS_VALIDATION.EMPTY')),
-    string().test('validate Address', i18n.global.t('CONNECT.ADDRESS_VALIDATION.NOT_THIS_NETWORK', {prefix: `cosmos or ${useConfigurationStore().config.addressPrefix}`}), (address: string | undefined) => {
-      if (!address) {
-        return false;
-      }
-      return address.startsWith('cosmos') || address.startsWith(useConfigurationStore().config.addressPrefix);
-    }),
-    string().test('validate Address', i18n.global.t(errorMessageType), validateAddress)
-  ])
-});
-
-watch(userLoggedIn, (next, prev)=> {
-  if(next){
-    address.value = useUserStore().getAccount.address;
-    useAirDropStore().fetchAirdrop(userStore.getAccount.address, true);
-    fetched.value=true;
-  }
-});
-
-async function submit(){
-  encodeAddress(address.value);
-}
+// const c4eAddress = ref();
+// const cosmosAddress = ref();
+// const addresses = computed({
+//   get() {
+//     return {
+//       c4e : c4eAddress.value,
+//       cosmos: cosmosAddress.value
+//     }
+//   },
+//   set(newVal){
+//     c4eAddress.value = encodeAddress(address.value);
+//     cosmosAddress.value = encodeAddress(address.value)
+//   }
+// })
+// watch(userLoggedIn, (next, prev)=> {
+//   if(next){
+//     address.value = useUserStore().getAccount.address;
+//     useAirDropStore().fetchAirdrop(userStore.getAccount.address, true);
+//     fetched.value=true;
+//   }
+// });
+//
+// async function submit(address: string){
+//   if(await validateAddress(address)){
+//     encodeAddress(address)
+//   }
+// }
 
 </script>
 
 <style scoped lang="scss">
 .airDrop{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(calc(100%/ 4), 1fr));
+  &__container{
+    background-color: white;
+    box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 10%);
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 0.5em;
+    border-radius: 5px;
+    grid-area: 1 / 2/ 1 / 4;
+    @media (max-width: 1024px) {
+      grid-area: 1 /1/ 1 / 5;
+    }
+  }
+  &__tabview{
+    width: 100%
+  }
+  hr{
+    color: gray;
+    font-size: 1.5em;
+    width: 80%;
+    align-items: center;
+    &::after{
+      content: attr(data-after);
+      color: black;
+      opacity: 1;
+      position: absolute;
+      margin-top: -23px;
+      margin-left: -20px;
+      background-color: white;
+      padding: 5px 20px
+    }
+  }
+  &__form{
+    width: 90%;
+  }
+  &__input{
+    margin-top: 30px
+  }
+  &__result{
+    width: 90%;
+    background-color: var(--bs-gray-400) ;
+    box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 10%);
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0.5em;
+    border-radius: 5px;
+  }
   &__login{
     display: flex;
     flex-direction: column;
@@ -214,15 +377,6 @@ async function submit(){
       display: grid;
       grid-template-columns: repeat(auto-fill, minmax(calc(100%/ 4), 1fr));
     }
-    &-form{
-      grid-area: 1 / 1 / 1 / 4;
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(calc(100%/ 4), 1fr));
-      align-items: center;
-      p{
-        margin: 0;
-      }
-    }
     &-keplr{
       grid-area: 1 / 4/ 1 / 4;
       display: flex;
@@ -233,22 +387,28 @@ async function submit(){
     }
   }
   &__text{
-    grid-area: 1 / 1/ 1 / 2;
+    margin-top: 1rem;
+    font-size: 15px;
   }
   &__field{
-    grid-area: 1 / 2/ 1 / 4;
+    max-width: 100%;
+    input{
+      border-radius: 5px;
+    }
+    span{
+      padding: 10px;
+    }
+
 
   }
   &__btn{
-    grid-area: 1 / 4/ 1 / 4;
-    padding: 10px;
+    width: 90% !important;
+    border-radius: 5px !important;
+
   }
   &__congrats{
     margin-top: 30px;
     padding: 20px
-  }
-  p{
-    margin: 0;
   }
 }
 
