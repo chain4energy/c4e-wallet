536a5994cbfb643f4932c1a246bd80e54d2f6e17
msiedlanowski <michal.siedlanowski@ovoo.pl>
2023-01-31 15:56:33 +0100

    show version on click

--- webapp/src/components/layout/AppFooter.vue
+++ webapp/src/components/layout/AppFooter.vue
@@ -5,20 +5,37 @@
     <span>Help</span>
     <router-link to="/terms_conditions"><span>Terms & Conditions</span></router-link>
     <router-link to="/privacy_policy"><span>Privacy Policy</span></router-link>
-    <span class="right">c4e</span>
-    <div class="hide">{{ app_version + "/" + compilation_timestamp}}</div>
+    <span ref="versionSpan" @click="showVersion=true" class="right">c4e</span>
+    <div ref="versionDiv" v-bind:class="{show: showVersion}" class="hide">{{ app_version + "/" + compilation_timestamp}}</div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {useConfigurationStore} from "@/store/configuration.store";
-import {onBeforeMount} from "vue";
+import {onBeforeMount, onMounted, ref} from "vue";
+import {loadFonts} from "@/plugins/webfontloader";
 
 const app_version = process.env.VUE_APP_VERSION;
 // const blockchain_version = process.env.VUE_APP_BLOCKCHAIN_VERSION;
 const compilation_timestamp = process.env.VUE_APP_COMPILATION_TIMESTAMP;
+const showVersion = ref<boolean>(false);
+const versionDiv = ref<HTMLDivElement>();
+const versionSpan = ref<HTMLDivElement>();
+
 onBeforeMount(() => {
   console.log(useConfigurationStore().config.airdropPoolAddress);
+
+
+});
+onMounted(() => {
+  const handleClickOutside = (event: MouseEvent) => {
+    if (!versionDiv.value || !versionSpan.value) return;
+    if (!versionDiv.value!.contains(event.target as Node) && !versionSpan.value!.contains(event.target as Node)) {
+      if(showVersion.value==true)
+        showVersion.value = false;
+    }
+  };
+  document.addEventListener('click', handleClickOutside);
 });
 
 </script>
@@ -51,17 +68,26 @@ onBeforeMount(() => {
     float: right;
     padding-right: 20px;
 
-    &:hover {
-      & + .hide {
-        width: 400px;
-        height: 40px;
-        position:absolute;
-        bottom:40px;
-        right:10px;
-        display:block;
-        background-color: white;
-      }
-    }
+    //&:hover {
+    //  & + .hide {
+    //    width: 400px;
+    //    height: 40px;
+    //    position:absolute;
+    //    bottom:40px;
+    //    right:10px;
+    //    display:block;
+    //    background-color: white;
+    //  }
+    //}
+  }
+  .show {
+    width: 400px;
+    height: 40px;
+    position:absolute;
+    bottom:40px;
+    right:10px;
+    display:block;
+    background-color: white;
   }
 }
 </style>
