f69178f1c2ae85cd7f1ef4d92a60fd5363818196
msiedlanowski <michal.siedlanowski@ovoo.pl>
2023-03-07 13:28:52 +0100

    BUG #4516

--- webapp/src/components/commons/AmountView.vue
+++ webapp/src/components/commons/AmountView.vue
@@ -5,18 +5,18 @@
       <div class="userdata-container" :class="useUserStore().isContinuousVestingAccount ? '' : 'width-95'">
         <div class="userdata-option" v-for="(items, index) in props.coins" :key="index">
           <span class="header" v-if="items.header">{{items.header}}</span>
-          <CoinAmount :amount="convertAmount(items.amount)" :precision="precision" :show-denom="items.showDenom || showDenom" :reduce-big-number="reduceBigNumber"/>
+          <CoinAmount :amount="convertAmount(items.amount)" :precision="precision" :show-denom="items.showDenom || showDenom" :show-tooltip="true" :reduce-big-number="reduceBigNumber"/>
         </div>
       </div>
       <span class="vesting-container" v-if="useUserStore().isContinuousVestingAccount && showVesting">
         <div class="vesting-flag">Vesting</div>
         <div class="userdata-option vesting-first" v-if="useUserStore().isContinuousVestingAccount">
               <span class="header">{{ $t('USER_DATA.LOCKED') }}</span>
-              <CoinAmount :key="locked" :amount="new BigIntWrapper(locked)" :show-denom="true"/>
+              <CoinAmount :key="locked" :amount="convertAmount(locked)" :precision="precision" :reduce-big-number="reduceBigNumber" :show-tooltip="true" :show-denom="true"/>
           </div>
           <div class="userdata-option vesting" v-if="useUserStore().isContinuousVestingAccount">
               <span class="header">{{ $t('USER_DATA.VESTING_END') }}</span>
-              <b><DateCommon :date="useUserStore().getAccount.continuousVestingData?.endTime"/></b>
+              <b><DateCommon :date="useUserStore().getAccount.continuousVestingData?.endTime" :show-time="false" :showTooltip="true" /></b>
           </div>
           <div class="userdata-option vesting" v-if="!useUserStore().isContinuousVestingAccount"></div>
 
--- webapp/src/components/commons/DateCommon.vue
+++ webapp/src/components/commons/DateCommon.vue
@@ -1,18 +1,38 @@
 <template>
-<span>{{trueDate}}</span>
+<span v-tooltip="{ value: retrieveConvertedTime(), disabled:!props.showTooltip, class:'coin-amount-tooltip'}">
+  {{trueDate}}
+</span>
 </template>
 
 <script setup lang="ts">
 import {computed} from "vue";
 
 const props = defineProps<{
-  date: Date
+  date: Date,
+  showTime: {
+    type: boolean,
+    default: true,
+    required: false
+  },
+  showTooltip: {
+    type : boolean,
+    required: false,
+    default: false
+  },
 }>();
 
 const trueDate = computed(()=> {
   let date = new Date(props.date);
+  if(!props.showTime) {
+    return date.toLocaleDateString();
+  }
   return date.toLocaleDateString() + ', ' + date.toLocaleTimeString();
 });
+
+const retrieveConvertedTime = () => {
+  let date = new Date(props.date);
+  return date.toLocaleDateString() + ', ' + date.toLocaleTimeString();
+};
 </script>
 
 <style scoped lang="scss">
