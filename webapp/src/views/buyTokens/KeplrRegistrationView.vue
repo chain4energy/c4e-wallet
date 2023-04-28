3ec2483a475c5cb7860a513044743f5c52e17d2c
msiedlanowski <michal.siedlanowski@ovoo.pl>
2023-04-28 12:53:56 +0200

    public sale - keplr registartion

--- webapp/src/views/buyTokens/KeplrRegistrationView.vue
+++ webapp/src/views/buyTokens/KeplrRegistrationView.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    Keplr registration
+  <div class="registration_container">
+    <h3>Keplr registration</h3>
+    <div>
+      Some information about...
+    </div>
+    <div>
+      <KeplrRegister />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import KeplrRegister from "@/components/buyTokens/KeplrRegister.vue";
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+.registration_container {
+  font-weight: 500;
+  text-align: start;
+  padding: 30px;
+  h3 {
+    font-weight: 700;
+  }
+}
 
 </style>
