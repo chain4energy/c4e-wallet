41d3d146fc1a8cb5f4c4761d23b89922fb83d07c
msiedlanowski <michal.siedlanowski@ovoo.pl>
2023-07-28 10:53:20 +0200

    new signing messages

--- webapp/src/config/message.ts
+++ webapp/src/config/message.ts
@@ -6,19 +6,19 @@ export default {
     'Address: {address}\n' +
     'Code: ' + '{activationCode}' + '\n\n' +
     'Nonce: ' + '{randomString}',
-  PAIRING_EMAIL_METAMASK_WALLET_MESSAGE: 'You are linking ethereum wallet address with your account.\n' +
+  PAIRING_EMAIL_METAMASK_WALLET_MESSAGE: 'You are linking EVM wallet address with your account.\n' +
     'Sign this message to prove you are the owner of this wallet.\n' +
     'This won\'t cost you anything.\n\n' +
     'Email: {email}\n' +
     'Address: {address}\n' +
     'Code: ' + '{activationCode}' + '\n\n' +
     'Nonce: ' + '{randomString}',
-  COSMOS_LOGIN_MESSAGE: 'You are linking C4E claimer wallet address with your account.\n' +
+  COSMOS_LOGIN_MESSAGE: 'You are currently logging into the C4E website.\n' +
     'Sign this message to prove you are the owner of this wallet.\n' +
     'This won\'t cost you anything.\n\n' +
     'Address: {address}\n\n' +
     'Nonce: ' + '{randomString}',
-  METAMASK_LOGIN_MESSAGE: 'You are linking ethereum wallet address with your account.\n' +
+  METAMASK_LOGIN_MESSAGE: 'You are currently logging into the C4E website.\n' +
     'Sign this message to prove you are the owner of this wallet.\n' +
     'This won\'t cost you anything.\n\n' +
     'Address: {address}\n' +
@@ -30,7 +30,7 @@ export default {
     'ClaimAddress: {ClaimAddress}\n' +
     'EthereumAddress: ' + '{EthereumAddress}' + '\n' +
     'Code: ' + '{activationCode}',
-  METAMASK_COSMOS_PAIRING_MESSAGE: 'You are linking ethereum wallet address with your account.\n' +
+  METAMASK_COSMOS_PAIRING_MESSAGE: 'You are linking EVM wallet address with your account.\n' +
     'Sign this message to prove you are the owner of this wallet.\n' +
     'This won\'t cost you anything.\n\n' +
     'Email: {email}\n' +
