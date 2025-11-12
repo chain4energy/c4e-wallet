311134a7ef210021c6620477260c2a1fef8a6d5a
Jakub Siemek <jakub.siemek@gmail.com>
2025-11-12 10:33:22 +0100

    fix issue in mainnet

--- webapp/src/router/before_each.ts
+++ webapp/src/router/before_each.ts
@@ -15,9 +15,11 @@ export function createRouterBeforeEach (logger: LoggerService) {
     }
     if(to.meta.requiresNotMainNetwork == true && !useConfigurationStore().config.faucetAvailable) {
       next('/');
+      return;
     }
     if(to.meta.isPublicSale == true && !useConfigurationStore().config.publicSaleVisible) {
       next('/');
+      return;
     }
     if (to.matched.some(record => record.meta.requiresAuth)) {
       if (useUserServiceStore().isLoggedIn) {
